018c55d7928d82ffd10d16c235bbfab251cacd03
lkirch <lkirch@users.noreply.github.com>
2025-08-31 20:18:14 +0000

    removed constants

--- config/__init__.py
+++ config/__init__.py
@@ -3,5 +3,4 @@ Config package initializer.
 Provides easy access to environment-based config and project constants.
 """
 
-from .config import *      # QDRANT_URL, OPENAI_API_KEY, COLLECTION_NAME, etc.
-from .constants import *   # EMBEDDING_DIM, DISTANCE_METRIC, CHUNK_SIZE, etc.
+from .config import *
